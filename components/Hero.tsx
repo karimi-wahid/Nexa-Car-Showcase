e06d09e17953c0b1fd03e99e75910b34452a4d50
Wahidullah Karimi <karimi.wahidullah22@gmail.com>
2025-05-28 10:16:35 +0430

    font size chnages

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
       <div className="flex-1 pt-36 sm:px-16 px-6">
-        <h1 className="2xl:text-[60px] sm:text-[50px] text-[40px] font-extrabold">
+        <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
           Discover, reserve, or rent your ideal car effortlessly today!
         </h1>
         <p className="text-[27px] text-black-100 font-light mt-5">
